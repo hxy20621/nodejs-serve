643ff0d7af983a1908749129d51b159258fd4266
NameHtz <breakname@163.com>
2020-10-21 17:38:55 +0800

    morgan

--- index.ts
+++ index.ts
@@ -7,10 +7,24 @@ import socket = require('socket.io')
 import {json} from 'body-parser';
 import routes from './routes';
 
+
+import morgan = require('morgan');
+
 const cors = require('cors');
 
+const fs = require('fs');
+const path = require('path');
+const dayjs = require("dayjs");
 const app = express();
 
+const logTime = dayjs(new Date()).format("YYYY-MM-DD")
+fs.mkdirSync(path.join(`${__dirname}/logs`))
+const accessLogStream = fs.createWriteStream(path.join(`${__dirname}/logs`, `${logTime}.log`), {flags: 'a'})
+morgan.token('localDate', (req) => {
+  return dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss SSS [Z] A')
+})
+morgan.format('combined', ':remote-addr - :remote-user [:localDate] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use(cors());
 
