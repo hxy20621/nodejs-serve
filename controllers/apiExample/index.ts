8ba8ddec606e51c91d43f123525681253c098e29
NameHtz <breakname@163.com>
2021-02-03 17:41:51 +0800

    modify sql config

--- controllers/apiExample/index.ts
+++ controllers/apiExample/index.ts
@@ -1,13 +1,24 @@
 import {RequestHandler, Request, Response, NextFunction} from 'express-serve-static-core';
+import conn from './../../models';
+import {MysqlError, FieldInfo} from 'mysql'
 
 const apiExample: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-  res.send(
-    {
-      status: 200,
-      data: req.body,
-      message: "Successful operation"
-    }
-  );
+    conn.query('select * from test', function (error: MysqlError, results: any, fields: FieldInfo) {
+        
+        if (error) res.send({
+            status: 200,
+            data: req.body,
+            message: "error"
+        })
+
+        res.send(
+            {
+                status: 200,
+                data: {results},
+                message: "Successful operation"
+            }
+        );
+    })
 }
 
 export default apiExample
